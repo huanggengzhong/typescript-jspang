ee0ddace82a3907f5f35b5ff4468da8548159f4e
huanggengzhong <915835528@qq.com>
2020-09-15 23:25:48 +0800

    备份

---
+++ 01-demo.ts
@@ -0,0 +1,20 @@
+const count:number=1;
+// count ="jspang";//报错
+
+
+
+// 自定义静态类型
+
+interface XiaoJieJie {
+  uname:string,
+  age:number
+}
+
+
+const xiaohong:XiaoJieJie={
+  uname:'小红',
+  age:18
+}
+
+console.log(xiaohong);// { uname: '小红', age: 18 }
+
---
+++ 02-基础静态类型和对象类型.ts
@@ -0,0 +1,38 @@
+
+// 基础静态类型
+const count:number=99
+const myName:string='jspang'
+
+
+// 对象类型
+
+const xiaojiejie:{name:string,age:number}={
+  name:'大脚',
+  age:30
+}
+console.log(xiaojiejie);//{ name: '大脚', age: 30 }
+
+// 对象类型也可以是数组
+
+const xiaojiejies:String[]=["刘英","小红"]//String大写
+
+const xiaojiejies2:Number[]=[1,2,3]
+
+// 类的形式定义变量
+
+class Person{}
+
+const dajiao:Person=new Person()
+console.log(dajiao,"类dajiao");
+
+
+// 函数形式
+// 下面意思要求函数,返回字符串
+
+const jianXiaojiejieFn:()=>string=()=>("大脚")
+console.log(jianXiaojiejieFn(),"func");
+
+
+
+const jianxiaojiejie:()=>string=()=>{return '111'}
+
