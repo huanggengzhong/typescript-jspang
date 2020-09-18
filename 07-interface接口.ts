bfdd14daa24f12b92ea0f6303dfaadfea5ccad35
huanggengzhong <huanggengzhong@daishujiankang.com>
2020-09-18 19:44:27 +0800

    上传

--- 07-interface接口.ts
+++ 07-interface接口.ts
@@ -51,4 +51,35 @@ const girl={
 }
 screenResume(girl)
 getResume(girl)
+ 
+//与数组对象别名的区别 :
+// 别名可以直接给类型,而接口必须是对象
 
+type Girl1=string//别名
+
+interface Girl { //接口
+    name: string,
+    age: number,
+    bust: number
+}
+
+// 接口里的非必填值
+
+// 比如下面腰围非必填
+
+interface Girl2{
+    name:string,
+    age:number,
+    bust:number,
+    waistline?:number
+}
+
+const getResume2=(girl:Girl2)=>{
+    console.log(girl.name+"年龄是:"+girl.age);
+    console.log(girl.name+"胸围是:"+girl.bust);
+    girl.waistline&&console.log(girl.name+"腰围是:"+girl.waistline);
+    
+    
+}
+getResume2({name:"小花",age:18,bust:89})
+getResume2({name:"小花",age:18,bust:89,waistline:90})
