e6b8944ccdfebd11699a24ac63975783198d0380
huanggengzhong <huanggengzhong@daishujiankang.com>
2020-10-14 16:30:15 +0800

    上传

--- 18.联合类型.ts
+++ 18.联合类型.ts
@@ -37,3 +37,32 @@ function judgeWho(animal: Waiter | Teacher) {
 
 
 //  类型保护的其它知识
+
+
+// 现在有一个需求:如果数字或者字符串让他们相加
+// function add(first: string | number, second: string | number) {
+//   return first + second;//报错了
+// }
+// 使用typeof语法解决这个问题
+
+function add(first: string | number, second: string | number) {
+  if (typeof first === 'string' || typeof second === 'string') {
+    return `${first}${second}`;
+  } else {
+    return first + second;
+  }
+}
+
+// 如果上面的参数是对象或者类
+
+class NumberObj {
+  count: number
+}
+
+function addObj(first: object | NumberObj, second: object | NumberObj) {
+  // return first.count + second.count;//报错
+  // 解决方法使用instanceof语法
+  if (first instanceof NumberObj && second instanceof NumberObj) {//注意instanceof 只能用在类上,并且这里是&&
+    return first.count + second.count;
+  } else return 0;
+}
