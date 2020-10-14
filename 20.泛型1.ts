e31148dbb3a0ca627cb7c99bf663077cad29308c
huanggengzhong <huanggengzhong@daishujiankang.com>
2020-10-14 17:28:05 +0800

    上传

--- 20.泛型1.ts
+++ 20.泛型1.ts
@@ -16,5 +16,30 @@ console.log(join<string>('jspang', '.com'));
 console.log(join<number>(1, 2));
 
 
+// 数组泛型的使用
+// 写法1:直接用T[]
+function myFun<T>(params: T[]) {//声明
+  return params;
+}
+
+console.log(myFun<string>(['123', "456"]));//使用,打印 '123', '456' ]
+
+// 写法2:用Array<T>
+
+function myFun2<T>(params: Array<T>) {
+  return params;
+}
+console.log(myFun2<string>(['123', '456']), "泛型写法2");
 
 
+// 多个泛型的定义
+
+function myFun3<T, P>(first: T, second: P) {
+  return `${first}${second}`
+}
+console.log(myFun3<number, string>(1, "2"), "多个泛型的定义");//"12"
+
+// 泛型的类型推断
+function myFun4<T, P>(first: T, second: P) {
+  return `${first}${second}`
+}
