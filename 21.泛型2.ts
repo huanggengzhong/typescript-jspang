741177dfdbeb7e45c5c88e835700713d51a2cd2e
huanggengzhong <huanggengzhong@daishujiankang.com>
2020-10-15 09:18:29 +0800

    泛型完成

--- 21.泛型2.ts
+++ 21.泛型2.ts
@@ -19,11 +19,52 @@
 //     return this.girls[index]
 //   }
 // }
-// 这样的写法不够优雅,使用泛型来标准化
-class SelectGirl<T>{
+// // 这样的写法不够优雅,使用泛型来标准化
+// class SelectGirl<T>{
+//   constructor(private girls: T[]) { }
+//   getGirl(index: number): T {
+//     return this.girls[index]
+//   }
+// }
+
+// // const selectGirl = new SelectGirl(['大脚', "刘英", "小红"])
+// // console.log(selectGirl.getGirl(1));//刘英
+// // 上面是类型推断出来的
+// const selectGirl = new SelectGirl<string>(['大脚', "刘英", "小红"])
+// console.log(selectGirl.getGirl(1));//刘英
+
+
+// 泛型中的继承
+// 需求要求数组对象,里面有name属性
+interface Girl {
+  name: string
+}
+
+class SelectGirl<T extends Girl>{
+  constructor(private girls: T[]) { }
+  getGirl(index: number): string {
+    return this.girls[index].name;
+  }
+}
+
+
+const selectGirl = new SelectGirl([
+  { name: "大脚" },
+  { name: "刘英" },
+  { name: "小红" }
+])
+
+console.log(selectGirl.getGirl(1));//刘英
+
+// 泛型约束:也就是继承来自比如对象、字符串、布尔、数字
+
+class SelectGirl2<T extends number | string | boolean | Object>{
   constructor(private girls: T[]) { }
   getGirl(index: number): T {
     return this.girls[index]
   }
 }
+
+const selectGirl2 = new SelectGirl2<string>(['大脚', "刘英", "小红"])
+console.log(selectGirl2.getGirl(1), "泛型约束");//刘英 泛型约束
 
